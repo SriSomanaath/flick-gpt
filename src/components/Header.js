2938244df495abf48d0ca211d6411f04ceb60adc
Sreenath <srinath@techarion.com>
2024-02-25 15:10:27 +0530

    profile setUp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { auth } from "../utils/firebase";
-// import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { signOut } from "firebase/auth";
 
 const Header = () => {
+  const user = useSelector((store) => store.user);
   const navigate = useNavigate();
 
   const handleSignOut = () => {
@@ -19,19 +20,20 @@ const Header = () => {
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
       <img className="w-44 mx-auto md:mx-0" src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png" alt="logo" />
-        <div className="flex p-2 justify-between">
-            <select
-              className="p-2 m-2 bg-gray-900 text-white"
-            >
-            </select>
-          <button
+        {user && (<div className="flex p-2 justify-between">
+            <button
             className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
-          >
+            >
           </button>
+          <img
+            className="hidden md:block w-12 h-12"
+            alt="usericon"
+            src={user?.photoURL}
+          />
           <button onClick={handleSignOut} className="font-bold text-white ">
             (Sign Out)
           </button>
-        </div>
+        </div>)}
     </div>
   )
 }
