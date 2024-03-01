b2c5fe339dd8de57fd1140f9c2f5bf8f027adefa
Sreenath <srinath@techarion.com>
2024-03-01 21:13:11 +0530

    Add background Trailer

--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -1,15 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const movieSlice = createSlice({
+const moviesSlice = createSlice({
   name: "movies",
-  initialState: {},
+  initialState: {
+    nowPlayingMovies: null,
+    trailerVideo: null,
+  },
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
+    },
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
     }
   },
 });
 
-export const { addNowPlayingMovies } = movieSlice.actions;
+export const { addNowPlayingMovies, addTrailerVideo } =
+  moviesSlice.actions;
 
-export default movieSlice.reducer;
+export default moviesSlice.reducer;
