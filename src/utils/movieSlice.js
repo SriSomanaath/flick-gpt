f003a82d76aff2b45d76826015d14ec5c34db1ac
Sreenath <srinath@techarion.com>
2024-03-01 23:26:55 +0530

    Secondary Container Filled with api and better ui

--- src/utils/movieSlice.js
+++ src/utils/movieSlice.js
@@ -4,19 +4,31 @@ const moviesSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
+    popularMovies: null,
     trailerVideo: null,
+    topRated:null,
+    upComingMovies:null
   },
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
+    addPopularMovies: (state, action) => {
+      state.popularMovies = action.payload;
+    },
+    addTopRatedMovies: (state, action) => {
+      state.topRated = action.payload;
+    },
     addTrailerVideo: (state, action) => {
       state.trailerVideo = action.payload;
-    }
+    },
+    addUpcomingMovies: (state, action) => {
+      state.upComingMovies = action.payload;
+    },
   },
 });
 
-export const { addNowPlayingMovies, addTrailerVideo } =
+export const { addNowPlayingMovies, addTrailerVideo, addPopularMovies, addTopRatedMovies, addUpcomingMovies } =
   moviesSlice.actions;
 
 export default moviesSlice.reducer;
