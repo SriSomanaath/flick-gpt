d1edc3cf9a6399f0616aae5f1b61fd7685d109c3
Sreenath <srinath@techarion.com>
2024-03-02 04:26:15 +0530

    SearchBar and Suggestion component and MultiLAnguage selections and GPT search component

---
+++ src/components/GptSearch.js
@@ -0,0 +1,19 @@
+import { BG_URL } from "../utils/constants";
+import GptSearchBar from './GptSearchBar';
+import GptMovieSuggestions from './GptMovieSuggestions';
+
+const GptSearch = () => {
+  return (
+    <>
+      <div className="fixed -z-10">
+        <img className="w-screen h-screen object-cover" src={BG_URL} alt="logo" />
+      </div>
+      <div className="">
+        <GptSearchBar />
+        <GptMovieSuggestions />
+      </div>
+    </>
+  )
+}
+
+export default GptSearch
